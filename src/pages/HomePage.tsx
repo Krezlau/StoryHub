8efcf9a5909559291d3b84fadf32fb3baa56b046
Krezlau <krzysztof.andrzej.jurkowski@gmail.com>
2023-01-02 13:57:12 +0100

    initial setup

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h2>Welcome to the Stories Website!</h2>
+      <p>Here you can find all the interesting stories posted by users.</p>
+    </div>
+  );
+};
+
+export default HomePage;
